3db2877d427319d3743d7f737f5e28efa475974b
yudiz-sahibe <sahibe.alam@yudiz.com>
2022-11-11 10:19:39 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -98,14 +98,21 @@ slider.addEventListener('mousemove', (e) => {
 
 
 
-document.addEventListener("mousemove", parallax);
-function parallax(e){
-  document.querySelectorAll(".object").forEach(function(move){
 
-    var moving_value = move.getAttribute("data-value");
-    var x = (e.clientX * moving_value) / 90;
-    var y = (e.clientY * moving_value) / 90;
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("mousemove", parallax);
+  function parallax(e){
+    document.querySelectorAll(".object").forEach(function(move){
+  
+      var moving_value = move.getAttribute("data-value");
+      var x = (e.clientX * moving_value) / 200;
+      var y = (e.clientY * moving_value) / 200;
+  
+      move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    });
+  }
+});
 
-    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
-  });
-}
+
+
+ 
